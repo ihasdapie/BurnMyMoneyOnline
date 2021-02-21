24d9c8169ffc3dd110f43ae3b44ec7f2a6509401
Brian Chen <brianchen.chen@mail.utoronto.ca>
2021-02-20 21:29:02 -0500

    add button, rocket

--- burnmymoney/src/Hero.js
+++ burnmymoney/src/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Hero = ({handleLogout}) => {
+const Hero = (props) => {
+
+    const { handleLogout, handle_buy } = props;
 
     return (
         <section className="hero">
@@ -8,8 +10,15 @@ const Hero = ({handleLogout}) => {
                 <h2>BurnMyMoney.Online</h2>
                 <button onClick={handleLogout}>Logout</button>
             </nav>
+            <div>
+                <button id = "bigFuckingButton" onClick={handle_buy}>BUY</button>
+            </div>
+
+
         </section>
     )
 }
+
+
 
 export default Hero;
