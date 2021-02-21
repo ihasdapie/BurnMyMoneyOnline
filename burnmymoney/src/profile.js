881333502afec6e99a373e3f5d1f3c8101b19c48
Jack Cai <caixunshiren@gmail.com>
2021-02-21 00:15:31 -0500

    1

--- burnmymoney/src/profile.js
+++ burnmymoney/src/profile.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Pie, Doughnut} from 'react-chartjs-2';
 
-function get_Portfolio(key_id, secret_key, url) {
+async function getAccount(alpaca) {
+    const account = await alpaca.getAccount();
+    return account;
+    
+  }
+
+function get_Portfolio(alpaca) {
+
+    
+    
     // returns portfolio and shares
-    var P_stockes = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-    var P_shares = [12, 19, 3, 5, 2, 3];
-    var colors = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-    var colors_hover = ["LightRed", "LightBlue", "LightYellow", "LightGreen", "LightPurple", "LightOrange"];
+    var P_stockes = ["GME", "NOK", "AMC", "VCNX", "UAMY", "WATT"];
+    var P_shares = [12001.45, 19001.33, 3340.6, 9659.2, 2017.7, 3320.2];
+    var colors = ["#5F9EA0", "#FFC0CB", "#E0FFFF", "#66CDAA", "#87CEFA", "#FF7F50"];
+    var colors_hover = ["#2F4F4F", "#2F4F4F", "#2F4F4F", "#2F4F4F", "#2F4F4F", "#2F4F4F"];
     
     var state = {
         labels: P_stockes,
@@ -27,34 +36,52 @@ function get_Portfolio(key_id, secret_key, url) {
 export default class Profile extends React.Component {
     constructor(props) {
         super(props)
-        this.key_id = 1;
-        this.secret_key = 2;
-        this.url = 3;
-        this.state = get_Portfolio(this.key_id, this.secret_key, this.url);
-
-        
+        //this.key_id = ;
+        //this.secret_key = ;
+        //const Alpaca = require('@alpacahq/alpaca-trade-api');
+        const alpaca = 1;//new Alpaca({
+        //    keyId: this.key_id,
+        //    secretKey: this.secret_key,
+         //   paper: true
+        //});
+        //var account = getAccount(alpaca);
+        //this.alpaca = alpaca;
+        //this.account = account;
+        //this.url = 3;
+        this.state = get_Portfolio(alpaca);
+        //this.buying_power = this.account.buying_power;
+        //console.log(account)
+        //console.log("123")
+        //console.log(account.buying_power)
       }
     
     
     render() {
     return (
       <div>
-
+        
         <Doughnut
           data={this.state}
           options={{
             title:{
               display:true,
-              text:'Your Stock Portfolio',
-              fontSize:20
+              fontColor: "White",
+              text: 'Your Portfolio',
+              fontSize:40
             },
             legend:{
               display:true,
-              position:'right'
+              position:'right',
+              labels: {
+                // This more specific font property overrides the global property
+                fontColor: 'White'
+                }
             }
           }}
         />
+        
       </div>
+      
     );
   }
 }
